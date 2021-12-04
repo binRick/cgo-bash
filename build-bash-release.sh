048ef8e73dd886e9b0e692040eacf66f2f2d1b46
root <root@f35.vpnservice.company>
2021-12-04 18:32:23 -0500

    m

--- build-bash-release.sh
+++ build-bash-release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 set -e
 cd $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+START_DIR="$(pwd)"
 DEFAULT_BASH_VERSION=3.2.57
 VERSION="${1:-$DEFAULT_BASH_VERSION}"
 [ "$VERSION" == "" ] && echo "build.sh: must specify bash version to download" && exit 1
@@ -34,24 +35,30 @@ fi
 	cd "${bashsrc}"
 	[ -f config.status ] || ./configure
 	make static
-  rsync ./libbash.a $LIB_DIR/libbash.a
   make strip
-  rsync ./bash $BIN_DIR/bash
-	ldflags=$(make ldflags | tail -1 | sed -E "s%\./%\${SRCDIR}/${bashsrc}/%g")
 
+set -x
+  rsync ./libbash.a $LIB_DIR/libbash.a
+  rsync ./bash $BIN_DIR/bash
+
+	ldflags=$(make ldflags | tail -1 | sed -E "s%\./%\${SRCDIR}/${bashsrc}/%g")
 	ldflags=$(
 		for ldflag in $ldflags; do
 			[ "${ldflag:0:1}" == "-" ] && echo -n "$ldflag " || echo -n "\${SRCDIR}/${bashsrc}/${ldflag} "
 		done
 	)
 
-#	gofmt -s >../c.go <<-EOF
-#		package bash
-#
-#		// #cgo LDFLAGS: ${ldflags}
-#		import "C"
-#	EOF
+#  if [[ ! -f "$START_DIR/c.go" ]]; then
+cat << EOF > $START_DIR/c.go
+      package bash
+
+      // #cgo LDFLAGS: ${ldflags}
+      import "C"
+EOF
+    gofmt -w $START_DIR/c.go
+#  fi
 )
 
 # always rebuild because Go doesn't know if C files/libraries have changed
+exit 0
 #go install -a -v
